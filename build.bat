d8ef4bc5023a95bc761fc7f2d82617a0646015b7
Sam Peddamatham <peddamat@gmail.com>
2022-05-30 02:08:41 -0700

    Added build.bat to allow Release builds from commandline.

---
+++ build.bat
@@ -0,0 +1,11 @@
+git clean -xfd
+
+msbuild -t:build -restore -p:RestorePackagesConfig=true /p:Configuration=Release /p:Platform=x64 PowerToys.sln
+
+msbuild -t:build -restore -p:RestorePackagesConfig=true /p:Configuration=Release /p:Platform=x64 tools\BugReportTool\BugReportTool.sln
+
+msbuild -t:build -restore -p:RestorePackagesConfig=true /p:Configuration=Release /p:Platform=x64 tools\WebcamReportTool\WebcamReportTool.sln
+
+msbuild -t:build -restore -p:RestorePackagesConfig=true /p:Configuration=Release /p:Platform=x64 tools\StylesReportTool\StylesReportTool.sln
+
+msbuild -t:build -restore -p:RestorePackagesConfig=true /p:Configuration=Release /p:Platform=x64 installer\PowerToysSetup.sln
